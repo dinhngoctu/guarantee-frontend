2135d6478d542d40fc29a40a1b97e1adefbf5d53
nvk <nvk3612@gmail.com>
2020-11-25 14:11:01 +0700

    fix header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavbarComponent
   ],
   imports: [
     BrowserModule,
